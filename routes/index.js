20b842de4b0c170e719b6b20a3cec6888879ea68
Timbo925 <twitters@vub.ac.be>
2015-01-24 20:23:07 +0100

    Dashboard Start

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var Address = require('../models/address')
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/dashboard', function(req, res) {
+  //TODO CHECK autentication
+
+  Address.find({user:req.user.id}, function(err,addrList) {
+     if (err) {return res.render('error', {error: err})}
+     var balance = 0;
+     for (var i = 0; i < addrList.length; i++) {
+        balance = balance + addrList[i].balanceSat
+     }
+     res.render('dashboard', { title: 'BitAlert Dashbaord',
+                               data:
+                                {balance:balance,
+                                 addr: addrList}});
+  })
 });
 
 module.exports = router;
